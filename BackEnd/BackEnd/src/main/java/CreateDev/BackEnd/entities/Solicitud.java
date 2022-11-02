f359879d8c1e22b46b101821ceea530f39e3da0e
PArts23 <alonso2405.13@gmail.com>
2022-11-01 23:35:08 -0500

    Se implementó la entidad Solicitud

---
+++ BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/entities/Solicitud.java
@@ -0,0 +1,82 @@
+package CreateDev.BackEnd.entities;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Solicitud-diseño")
+public class Solicitud {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    /*@OneToOne
+    @JoinColumn(name = "idCliente", nullable = false)
+    private Cliente cliente;
+
+    @OneToOne
+    @JoinColumn(name = "idArtesano", nullable = false)
+    private Artesano artesano;*/
+
+    @OneToOne
+    @JoinColumn(name = "idPedido", nullable = false)
+    private Pedido pedido;
+
+    @Column(name = "fecha")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private LocalDate fecha;
+
+    public Solicitud(){
+    }
+
+    public Solicitud(int id, LocalDate fecha) {
+        this.id = id;
+        //this.cliente = cliente;
+        //this.artesano = artesano;
+        this.fecha = fecha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /*public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Artesano getArtesano() {
+        return artesano;
+    }
+
+    public void setArtesano(Artesano artesano) {
+        this.artesano = artesano;
+    }*/
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+}
