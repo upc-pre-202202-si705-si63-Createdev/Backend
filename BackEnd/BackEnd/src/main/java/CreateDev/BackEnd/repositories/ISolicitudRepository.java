232d40838ab2d6e213d88a487268f58ae07abd66
PArts23 <alonso2405.13@gmail.com>
2022-11-01 23:40:21 -0500

    Se implementó el repositorio para Solicitud

---
+++ BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/repositories/ISolicitudRepository.java
@@ -0,0 +1,18 @@
+package CreateDev.BackEnd.repositories;
+
+import CreateDev.BackEnd.entities.Solicitud;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface ISolicitudRepository {
+
+    @Query("from Solicitud s where s.pedido.nombre like %:nombre%")
+    List<Solicitud> buscarPedido(@Param("nombre") String nombre);
+
+    List<Solicitud> findByfecha(Date fecha);
+}
